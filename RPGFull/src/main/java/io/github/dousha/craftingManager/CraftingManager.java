c149a502a2e19f709d0cd8668fe7f8dea0478ae7
dousha <lijiahao34@gmail.com>
2015-08-14 18:42:23 +0800

    init - not completed yet

--- RPGFull/src/main/java/io/github/dousha/craftingManager/CraftingManager.java
+++ RPGFull/src/main/java/io/github/dousha/craftingManager/CraftingManager.java
@@ -5,7 +5,12 @@ import io.github.dousha.rpgFull.utils.Pair;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.inventory.Inventory;
+
 public class CraftingManager {
-	// sink
-	List<Pair<String, ItemStruct>> _itemSink = new ArrayList<Pair<String, ItemStruct>>();
+	private List<Pair<String, ItemStruct>> _itemSink = new ArrayList<Pair<String, ItemStruct>>();
+	
+	private double calculateRate(Inventory i){
+		return 0.0;
+	}
 }
