bf2d5dd149822f2cc422f8c9718040876a3172a4
dousha <lijiahao34@live.com>
2020-02-29 17:33:48 +0800

    example plugin for PlayerData

--- BetterJoinEvent/src/main/java/tech/dsstudio/minecraft/betterjoin/Main.java
+++ BetterJoinEvent/src/main/java/tech/dsstudio/minecraft/betterjoin/Main.java
@@ -1,4 +1,45 @@
 package tech.dsstudio.minecraft.betterjoin;
 
-public class Main {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import tech.dsstudio.minecraft.betterjoin.events.PlayerRejoinEvent;
+import tech.dsstudio.minecraft.playerdata.PlayerData;
+import tech.dsstudio.minecraft.playerdata.driver.PlayerDataStorage;
+import tech.dsstudio.minecraft.playerdata.events.StorageReadyEvent;
+
+public class Main extends JavaPlugin implements Listener {
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@EventHandler
+	public void onStorageReady(StorageReadyEvent e) {
+		this.storage = e.getStorage();
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		if (storage != null) {
+			PlayerData data = storage.get(e.getPlayer().getUniqueId());
+			Object obj = data.get(LAST_TIME_JOIN_KEY);
+			PlayerRejoinEvent event;
+			if (obj != null) {
+				getLogger().info("Hello again!");
+				long lastTime = (long) obj;
+				event = new PlayerRejoinEvent(e, false, lastTime);
+			} else {
+				getLogger().info("Hello!");
+				event = new PlayerRejoinEvent(e, true, 0L);
+			}
+			data.set(LAST_TIME_JOIN_KEY, System.currentTimeMillis());
+			getServer().getPluginManager().callEvent(event);
+		}
+	}
+
+	private PlayerDataStorage storage = null;
+	private static final String LAST_TIME_JOIN_KEY = "btjLastJoin";
 }
--- BetterJoinEvent/src/main/java/tech/dsstudio/minecraft/betterjoin/events/PlayerRejoinEvent.java
+++ BetterJoinEvent/src/main/java/tech/dsstudio/minecraft/betterjoin/events/PlayerRejoinEvent.java
@@ -1,4 +1,39 @@
 package tech.dsstudio.minecraft.betterjoin.events;
 
-public class PlayerRejoinEvent {
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerRejoinEvent extends Event {
+	public PlayerRejoinEvent(PlayerJoinEvent e, boolean isFirstJoin, long lastJoinTimestamp) {
+		this.event = e;
+		this.isFirstJoin = isFirstJoin;
+		this.lastJoinTimeMills = lastJoinTimestamp;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return HANDLER_LIST;
+	}
+
+	public static HandlerList getHandlerList() {
+		return HANDLER_LIST;
+	}
+
+	public PlayerJoinEvent getEvent() {
+		return event;
+	}
+
+	public long getLastJoinTimeMills() {
+		return lastJoinTimeMills;
+	}
+
+	public boolean isFirstJoin() {
+		return isFirstJoin;
+	}
+
+	private static HandlerList HANDLER_LIST = new HandlerList();
+	private PlayerJoinEvent event;
+	private boolean isFirstJoin;
+	private long lastJoinTimeMills;
 }
